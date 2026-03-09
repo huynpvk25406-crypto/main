bf1dd104c529919862d449e93c9f1af22c6635cf
Soo1511 <sontdk25406@st.uel.edu.vn>
2026-03-09 12:26:29 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-from code import DangNhap
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = DangNhap()
-    w.show()
-    sys.exit(app.exec())
